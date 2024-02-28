02ad54f2dba637f95735c0d037d68e407ac905ac
Alex <Alex.souza12@live.com>
2024-02-28 18:30:13 -0300

    Adicinado a ladding-page

--- src/pages/ladding-page/index.js
+++ src/pages/ladding-page/index.js
@@ -1,26 +1,15 @@
 import "./index.scss";
+import Logo from '../../assets/images/logo.png'
 import { Link } from 'react-router-dom';
 import React  from "react";
 export default function Home(){
     return(
         <section className="pag-home">
             <div className="sec1">
-                <h1>Ladding page</h1>
-                <Link to='/cadastro'>Cadastro</Link>
                 
-                <Link to='/login'>Login</Link>
-               
-                <Link to='/recuperar'>Recuperar Senha</Link>
+                <img src={Logo} alt="Logo da SneakStyle"></img>
                 
-                <Link to='/compra'>Shopping</Link>
-               
-                <Link to='/listprod'>Lista Produtos</Link>
-               
-                <Link to='/carrinho'>Carrinho</Link>
-                
-                <Link to='/produto'>Produto</Link>
-                
-                <Link to='/cadprodutos'>Cadastro de Produto</Link>                
+                                
             </div>
         </section>
     )

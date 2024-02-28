1398ca2cb53ca12e15ad536f6a601d343a6618af
Alex <Alex.souza12@live.com>
2024-02-28 19:15:07 -0300

    Revert "Adicionado Link"
    This reverts commit bdc49164d7128f4f58f221d4cc465ea606e6cdc2.

--- src/pages/ladding-page/index.js
+++ src/pages/ladding-page/index.js
@@ -2,13 +2,12 @@ import "./index.scss";
 import Logo from '../../assets/images/logo.png'
 import { Link } from 'react-router-dom';
 import React  from "react";
-import Cadastro from "../page-cad";
 export default function Home(){
     return(
         <section className="pag-home">
             <div className="sec1">
+                
                 <img src={Logo} alt="Logo da SneakStyle"></img>
-                <Link to='/cadastro'>Cadastro</Link>
                 
                                 
             </div>
